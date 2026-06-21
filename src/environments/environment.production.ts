a7ec5a5b915ba81f671b0442667dc605a9f18d62
prabhakarkoranga41 <prabhakarkoranga41@gmail.com>
2026-06-22 00:09:58 +0530

    added fast api azure URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://tracknest-api-grbfe3ascsdsh6c0.malaysiawest-01.azurewebsites.net',
-  aiApiUrl: 'https://tracknest-ai.azurewebsites.net'
+  aiApiUrl: 'https://tracknest-fastapi-gnhjcafkayf9hwfy.westcentralus-01.azurewebsites.net'
 };
